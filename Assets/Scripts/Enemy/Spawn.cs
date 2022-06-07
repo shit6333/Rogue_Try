e5267fa09a48407439e433fe479b3b6f80d1d62f
Ricky <shit6333@gmail.com>
2022-06-08 02:38:17 +0800

    Add New Enemy [bug]

--- Assets/Scripts/Enemy/Spawn.cs
+++ Assets/Scripts/Enemy/Spawn.cs
@@ -3,15 +3,26 @@ using System.Collections;
 
 public class Spawn : MonoBehaviour
 {
-    public GameObject spawnPrefab;
+    public GameObject[] spawnEnemys;    // Enemy List
+
     public float spawnTime = 1f;
-    public int generateAmount = 2;   // 生成數量 range
+    public int generateMaxAmount = 2;   // 生成數量 range
     public float generateMaxRange = 3f; // 生成位置 range
     
     private float generatePosX;
     private float generatePosY;
     private int prefabNum = 0;      // 生成物件數量
     private bool isSpawn = true;   // 是否生成
+    private GameObject spawnEnemy;  // 要生成的 Enemy
+    private int spawnCount = 0;     // 所有 Enemy 種類數量
+    private int leastSpawnNum = 0;  // 最少一次生成數量
+    private int generateAmount = 0;
+
+    private void Start()
+    {
+        spawnCount = spawnEnemys.Length;
+        generateAmount = generateMaxAmount;
+    }
 
     private void FixedUpdate()
     {
@@ -26,14 +37,30 @@ public class Spawn : MonoBehaviour
     }
 
     private void GeneratePrefab(){
+        // 隨機選擇要產生的 Enemy
+        int enemyIndex = Random.Range(0, spawnCount);
+        spawnEnemy = spawnEnemys[enemyIndex];
+
+        // 不同 Enemy 生成數量 range
+        if (enemyIndex == 0)
+        {
+            generateAmount = generateMaxAmount;
+            leastSpawnNum = 0;
+        }
+        if (enemyIndex == 1)
+        {
+            leastSpawnNum = 3;          // 以後還會做更動 !!!!!!!!!!!!!!!!!!!
+            generateAmount = 10;       //  TODO : 用成變數或另外設置
+        }
+
         // 生成隨機數量
-        prefabNum = Random.Range(0,generateAmount);
+        prefabNum = Random.Range(leastSpawnNum, generateAmount);
 
         for (int i = 0; i < prefabNum; i++)
         {
             generatePosX = Random.Range(-generateMaxRange, generateMaxRange);
             generatePosY = Random.Range(-generateMaxRange, generateMaxRange);
-            Instantiate(spawnPrefab, new Vector3( transform.position.x + generatePosX,
+            Instantiate(spawnEnemy, new Vector3( transform.position.x + generatePosX,
                                                   transform.position.y + generatePosY, 0), Quaternion.identity);
         }
     }
